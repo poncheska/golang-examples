b13d20ffa65f115d35868e4cae83f5b7b7f5fede
Aleksey <aleksei_kovgar@mail.ru>
2021-01-09 19:12:44 +0300

    add image

--- image/image.go
+++ image/image.go
@@ -22,12 +22,23 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
 	imgs := RepeatedSmoothImage(img, 200)
 	ff, err := os.Create("smothed_image.png")
 	if err != nil {
 		panic(err)
 	}
 	err = png.Encode(ff, imgs)
+	if err != nil {
+		panic(err)
+	}
+
+	imgfs := FastSmoothImage(img, 10)
+	ff, err = os.Create("fast_smothed_image.png")
+	if err != nil {
+		panic(err)
+	}
+	err = png.Encode(ff, imgfs)
 	if err != nil {
 		panic(err)
 	}
@@ -47,14 +58,14 @@ func SmoothImage(img0 image.Image) image.Image {
 		for i := bounds.Min.X; i < bounds.Max.X; i++ {
 			for j := bounds.Min.Y; j < bounds.Max.Y; j++ {
 				n = 1
-				r, g, b, a := img.At(i, j).RGBA()
+				r, g, b, a := img0.At(i, j).RGBA()
 				ri := int64(r)
 				gi := int64(g)
 				bi := int64(b)
 				ai := int64(a)
 				if i != bounds.Min.X {
 					n += 1
-					rn, gn, bn, an := img.At(i-1, j).RGBA()
+					rn, gn, bn, an := img0.At(i-1, j).RGBA()
 					ri += int64(rn)
 					gi += int64(gn)
 					bi += int64(bn)
@@ -62,7 +73,7 @@ func SmoothImage(img0 image.Image) image.Image {
 				}
 				if i != bounds.Max.X {
 					n += 1
-					rn, gn, bn, an := img.At(i+1, j).RGBA()
+					rn, gn, bn, an := img0.At(i+1, j).RGBA()
 					ri += int64(rn)
 					gi += int64(gn)
 					bi += int64(bn)
@@ -70,7 +81,7 @@ func SmoothImage(img0 image.Image) image.Image {
 				}
 				if i != bounds.Min.Y {
 					n += 1
-					rn, gn, bn, an := img.At(i, j-1).RGBA()
+					rn, gn, bn, an := img0.At(i, j-1).RGBA()
 					ri += int64(rn)
 					gi += int64(gn)
 					bi += int64(bn)
@@ -78,7 +89,7 @@ func SmoothImage(img0 image.Image) image.Image {
 				}
 				if i != bounds.Max.Y {
 					n += 1
-					rn, gn, bn, an := img.At(i, j+1).RGBA()
+					rn, gn, bn, an := img0.At(i, j+1).RGBA()
 					ri += int64(rn)
 					gi += int64(gn)
 					bi += int64(bn)
@@ -98,4 +109,50 @@ func RepeatedSmoothImage(img image.Image, rn int) image.Image {
 		img = SmoothImage(img)
 	}
 	return img
+}
+
+func FastSmoothImage(img0 image.Image, r int) image.Image {
+	img := img0
+	bounds := img.Bounds()
+	if cimg, ok := img.(Changeable); ok {
+		for i := bounds.Min.X; i < bounds.Max.X; i++ {
+			for j := bounds.Min.Y; j < bounds.Max.Y; j++ {
+				r, g, b, a := AvgRGBA(img0, MaxInt(i-r, bounds.Min.X), MaxInt(j-r, bounds.Min.Y),
+					MinInt(i+r, bounds.Max.X), MinInt(j+r, bounds.Max.Y))
+				cimg.Set(i, j, color.RGBA{Scale32To8(r),
+					Scale32To8(g), Scale32To8(b),
+					Scale32To8(a)})
+			}
+		}
+	}
+	return img
+}
+
+func AvgRGBA(img image.Image, x0, y0, x1, y1 int) (uint32, uint32, uint32, uint32) {
+	var r, g, b, a, n int64
+	for i := x0; i < x1; i++ {
+		for j := y0; j < y1; j++ {
+			n += 1
+			rn, gn, bn, an := img.At(i, j).RGBA()
+			r += int64(rn)
+			g += int64(gn)
+			b += int64(bn)
+			a += int64(an)
+		}
+	}
+	return uint32(r / n), uint32(g / n), uint32(b / n), uint32(a / n)
+}
+
+func MaxInt(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func MinInt(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
